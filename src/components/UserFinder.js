1dabc4a230a6165f92c68e528a1165d1f159d2d2
Fang Limin <fanglimin@Fangs-MacBook-Air.local>
2021-09-09 14:06:24 +0800

    context

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -2,28 +2,34 @@ import {Fragment, Component} from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css'
+import UsersContext from "../context/users-context";
 
-const DUMMY_USERS = [
-    { id: "u1", name: "Max" },
-    { id: "u2", name: "Manuel" },
-    { id: "u3", name: "Julie" },
-];
+// const DUMMY_USERS = [
+//     { id: "u1", name: "Max" },
+//     { id: "u2", name: "Manuel" },
+//     { id: "u3", name: "Julie" },
+// ];
 
 
 class UserFinder extends Component {
+    static contextType = UsersContext;
 
-    constructor() {
-        super();
-        this.state = {
-            searchTerm: '',
-            filteredUsers: DUMMY_USERS
-        };
-    }
+   state = {    searchTerm: '',
+                filteredUsers: this.context.users
+            };
+   //It seems context is not ready in constructor==> change to above!!!
+   //  constructor() {
+   //      super();
+   //      this.state = {
+   //          searchTerm: '',
+   //          filteredUsers: this.context.users
+   //      };
+   //  }
 
     searchChangeHandler = (event) => {
         this.setState({
             searchTerm: event.target.value,
-            filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(event.target.value))
+            filteredUsers: this.context.users.filter((user) => user.name.includes(event.target.value))
         })
     }
 
